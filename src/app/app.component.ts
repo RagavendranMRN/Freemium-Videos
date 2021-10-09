53d42ca0f23b00027d83e2049a060dab5b34eb30
RagavendranMRN <ragavendran1371997@gmail.com>
2021-10-09 18:25:04 +0530

    Added Search Results and few urls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,11 @@ export class AppComponent {
     'https://www.pexels.com/search/videos/$search/',
     'https://dareful.com/?s=$search',
     'https://www.youtube.com/results?search_query=$search&sp=EgIwAQ%253D%253D',
+    'https://coverr.co/s?q=$search',
+    'https://www.videvo.net/search/$search/',
+    'https://www.videezy.com/free-video/$search',
+    'https://mazwai.com/search/$search/',
+    'https://lifeofvids.com/search/$search',
   ];
   addNewEnable() {
     this.addFlag = true;
@@ -28,19 +33,26 @@ export class AppComponent {
     if (this.NewUrl != '' && this.NewSearch != '') {
       let newURL = this.NewUrl.replace(this.NewSearch, '$search');
       this.videoList.push(newURL);
+      console.log(newURL);
     }
     this.addFlag = false;
   }
   generateUrls() {
     this.values = [];
+    this.videoList.sort();
     this.videoList.map((x) => {
       let imageUrl = x.split('/');
       let image = 'http://www.google.com/s2/favicons?domain=' + imageUrl[2];
+      const result = imageUrl[2].split('.').filter((word) => {
+        if (word != 'www' && word != 'com') {
+          return name;
+        }
+      });
       let url = x.replace('$search', this.SearchQuery);
       this.values.push({
         url: url,
         image: image,
-        name: url,
+        name: result[0].toUpperCase(),
       });
     });
   }
